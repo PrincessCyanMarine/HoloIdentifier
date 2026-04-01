c36b1664da061333491f21462c54e6bb2a5a831e
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2026-03-31 21:30:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+const holomem = [["https://hololive.hololivepro.com/wp-content/uploads/2021/05/tokino_sora_thumb.png","Tokino Sora\nときのそら"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Robocosan_list_thumb.png","Robocosan\nロボ子さん"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Aki-Rosenthal_list_thumb.png","Aki Rosenthal\nアキ・ローゼンタール"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Akai-Haato_list_thumb.png","Akai Haato\n赤井はあと"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Shirakami-Fubuki_list_thumb.png","Shirakami Fubuki\n白上フブキ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Natsuiro-Matsuri_list_thumb.png","Natsuiro Matsuri\n夏色まつり"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Nakiri-Ayame_list_thumb.png","Nakiri Ayame\n百鬼あやめ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Yuzuki-Choco_list_thumb.png","Yuzuki Choco\n癒月ちょこ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Oozora-Subaru_list_thumb.png","Oozora Subaru\n大空スバル"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/AZKi_list_thumb.png","AZKi\nAZKi"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Ookami-Mio_thumb.png","Ookami Mio\n大神ミオ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Sakura-Miko_list_thumb.png","Sakura Miko\nさくらみこ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Nekomata-Okayu_list_thumb.png","Nekomata Okayu\n猫又おかゆ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Inugami-Korone_list_thumb.png","Inugami Korone\n戌神ころね"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Hoshimachi-Suisei_list_thumb.png","Hoshimachi Suisei\n星街すいせい"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Usada-Pekora_list_thumb.png","Usada Pekora\n兎田ぺこら"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Shiranui-Flare_list_thumb.png","Shiranui Flare\n不知火フレア"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Shirogane-Noel_list_thumb.png","Shirogane Noel\n白銀ノエル"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Houshou-Marine_list_thumb.png","Houshou Marine\n宝鐘マリン"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Tsunomaki-Watame_list_thumb.png","Tsunomaki Watame\n角巻わため"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Tokoyami-Towa_list_thumb.png","Tokoyami Towa\n常闇トワ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Himemori-Luna_list_thumb.png","Himemori Luna\n姫森ルーナ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Yukihana-Lamy_list_thumb.png","Yukihana Lamy\n雪花ラミィ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Momosuzu-Nene_list_thumb.png","Momosuzu Nene\n桃鈴ねね"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Shishiro-Botan_list_thumb.png","Shishiro Botan\n獅白ぼたん"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Omaru-Polka_list_thumb.png","Omaru Polka\n尾丸ポルカ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/La-Darknesss_list_thumb.png","La+ Darknesss\nラプラス・ダークネス"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Takane-Lui_list_thumb.png","Takane Lui\n鷹嶺ルイ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Hakui-Koyori_list_thumb.png","Hakui Koyori\n博衣こより"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Kazama-Iroha_list_thumb.png","Kazama Iroha\n風真いろは"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Sakamata-Chloe_list_thumb.png","[Affiliate] Sakamata Chloe\n沙花叉クロヱ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ayunda-Risu_list_thumb.png","Ayunda Risu\nアユンダ・リス"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Moona-Hoshinova_list_thumb.png","Moona Hoshinova\nムーナ・ホシノヴァ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Airani-Iofifteen_list_thumb.png","Airani Iofifteen\nアイラニ・イオフィフティーン"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Kureiji-Ollie_list_thumb.png","Kureiji Ollie\nクレイジー・オリー"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Anya-Melfissa_list_thumb.png","Anya Melfissa\nアーニャ・メルフィッサ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Pavolia-Reine_list_thumb.png","Pavolia Reine\nパヴォリア・レイネ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Vestia-Zeta_list_thumb.png","Vestia Zeta\nベスティア・ゼータ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Kaela-Kovalskia_list_thumb.png","Kaela Kovalskia\nカエラ・コヴァルスキア"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Kobo-Kanaeru_list_thumb.png","Kobo Kanaeru\nこぼ・かなえる"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Mori-Calliope_list_thumb.png","Mori Calliope\n森カリオペ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Takanashi-Kiara_list_thumb.png","Takanashi Kiara\n小鳥遊キアラ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ninomae-Inanis_list_thumb.png","Ninomae Ina’nis\n一伊那尓栖"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Watson-Amelia_list_thumb.png","[Affiliate] Watson Amelia\nワトソン・アメリア"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/IRyS_list_thumb.png","IRyS\nIRyS"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Ouro-Kronii_list_thumb.png","Ouro Kronii\nオーロ・クロニー"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Hakos-Baelz_list_thumb.png","Hakos Baelz\nハコス・ベールズ"],["https://hololive.hololivepro.com/wp-content/uploads/2021/07/Shiori-Novella_list_thumb.png","Shiori Novella\nシオリ・ノヴェラ"],["https://hololive.hololivepro.com/wp-content/uploads/2021/07/Koseki-Bijou_list_thumb.png","Koseki Bijou\n古石ビジュー"],["https://hololive.hololivepro.com/wp-content/uploads/2021/07/Nerissa-Ravencroft_list_thumb.png","Nerissa Ravencroft\nネリッサ・レイヴンクロフト"],["https://hololive.hololivepro.com/wp-content/uploads/2021/07/Fuwawa-Abyssgard_list_thumb.png","Fuwawa Abyssgard\nフワワ・アビスガード"],["https://hololive.hololivepro.com/wp-content/uploads/2021/07/Mococo-Abyssgard_list_thumb.png","Mococo Abyssgard\nモココ・アビスガード"],["https://hololive.hololivepro.com/wp-content/uploads/2023/07/Elizabeth-Rose-Bloodflame_list_thumb.png","Elizabeth Rose Bloodflame\nエリザベス・ローズ・ブラッドフレイム"],["https://hololive.hololivepro.com/wp-content/uploads/2023/07/Gigi-Murin_list_thumb.png","Gigi Murin\nジジ・ムリン"],["https://hololive.hololivepro.com/wp-content/uploads/2023/07/Cecilia-Immergreen_list_thumb.png","Cecilia Immergreen\nセシリア・イマーグリーン"],["https://hololive.hololivepro.com/wp-content/uploads/2023/07/Raora-Panthera_list_thumb.png","Raora Panthera\nラオーラ・パンテーラ"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Otonose-Kanade_list_thumb.png","Otonose Kanade\n音乃瀬奏"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Ichijou-Ririka_list_thumb.png","Ichijou Ririka\n一条莉々華"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Juufuutei-Raden_list_thumb.png","Juufuutei Raden\n儒烏風亭らでん"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Todoroki-Hajime_list_thumb.png","Todoroki Hajime\n轟はじめ"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Isaki-Riona_list_thumb.png","Isaki Riona\n響咲リオナ"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Koganei-Niko_list_thumb.png","Koganei Niko\n虎金妃笑虎"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Mizumiya-Su_list_thumb.png","Mizumiya Su\n水宮枢"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Rindo-Chihaya_list_thumb.png","Rindo Chihaya\n輪堂千速"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Kikirara-Vivi_list_thumb.png","Kikirara Vivi\n綺々羅々ヴィヴィ"],["https://hololive.hololivepro.com/wp-content/uploads/2025/09/Izuki-Michiru_list_thumb.png","Izuki Michiru\n井月みちる"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Hanazono-Sayaka_list_thumb.png","Hanazono Sayaka\n花園さやか"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Hanazono-Sayaka_list_thumb-1.png","Kazeshiro Yuki\n風白ゆき"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Minato-Aqua_list_thumb.png","[Alum] Minato Aqua\n湊あくあ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Murasaki-Shion_list_thumb.png","[Alum] Murasaki Shion\n紫咲シオン"],["https://hololive.hololivepro.com/wp-content/uploads/2020/06/Amane-Kanata_list_thumb.png","[Alum] Amane Kanata\n天音かなた"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Kiryu-Coco_list_thumb.png","[Alum] Kiryu Coco\n桐生ココ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Gawr-Gura_list_thumb.png","[Alum] Gawr Gura\nがうる・ぐら"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Tsukumo-Sana_list_thumb.png","[Alum] Tsukumo Sana\n九十九佐命"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Ceres-Fauna_list_thumb.png","[Alum] Ceres Fauna\nセレス・ファウナ"],["https://hololive.hololivepro.com/wp-content/uploads/2020/07/Nanashi-Mumei_list_thumb.png","[Alum] Nanashi Mumei\n七詩ムメイ"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Hiodoshi-Ao_list_thumb.png","[Alum] Hiodoshi Ao\n火威青"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Harusaki-Nodoka_list_thumb.png","[Retirement] Harusaki Nodoka\n春先のどか"],["https://hololive.hololivepro.com/wp-content/uploads/2023/09/Friend-A_list_thumb.png","[Retirement] Friend A (A-chan)\n友人A（えーちゃん）"]]
+let pos = 0;
+
+const pic = document.getElementById('pic');
+const name = document.getElementById('name');
+const revealButton = document.getElementById('reveal')
+const rightButton = document.getElementById('right')
+const resultsButton = document.getElementById('results')
+const wrongButton = document.getElementById('wrong')
+pic.src = holomem[pos][0]
+name.innerText =  holomem[pos][1]
+var nextPic = document.createElement('img')
+nextPic.src = holomem[++pos][0];
+nextPic.style.display = 'none'
+document.body.appendChild(nextPic);
+
+console.debug(holomem.length)
+
+var gotRight = 0;
+var gotWrong = 0;
+
+let revealed = false;
+
+const next = () => {
+    if (pos >= holomem.length) {
+        alert(`You got ${gotRight} right and ${gotWrong} wrong`);
+        rightButton.style.display = 'none'
+        wrongButton.style.display = 'none'
+        resultsButton.style.display = ''
+        return true;
+    }
+    revealed = false;
+    pic.src = nextPic.src;
+    name.style.display = 'none'
+    rightButton.style.display = 'none'
+    wrongButton.style.display = 'none'
+    revealButton.style.display = ''
+    name.innerText = holomem[pos][1];
+    if (pos < holomem.length) nextPic.src = holomem[++pos][0];
+    return false;
+}
+
+const reveal = () => {
+    name.style.display = ''
+    rightButton.style.display = ''
+    wrongButton.style.display = ''
+    revealButton.style.display = 'none'
+    revealed = true;
+}
+
+const results = next;
+
+const right = () => {
+    gotRight++;
+    next()
+}
+const wrong = () => {
+    gotWrong++;
+    next();
+}
+
+document.addEventListener('keydown', ({key}) => {
+    console.debug(key);
+    switch (key) {
+        case 'ArrowLeft':
+            if (revealed) wrong();
+            else reveal();
+            break;
+        case 'ArrowRight':
+            if (revealed) right();
+            else reveal();
+            break;
+        case ' ':
+            if (!revealed) reveal();
+            break;
+    }
+})
